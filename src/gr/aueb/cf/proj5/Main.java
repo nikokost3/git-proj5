957edb2bec5c321d220f94f98e386608a1fc387b
nikokost3 <nikoskos26@yahoo.gr>
2024-02-16 11:54:04 +0200

    sayHello methods

--- src/gr/aueb/cf/proj5/Main.java
+++ src/gr/aueb/cf/proj5/Main.java
@@ -3,11 +3,15 @@ package gr.aueb.cf.proj5;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello Coding Factory!!!");
+        helloCF();
         sayHello();
     }
 
     public static void sayHello() {
         System.out.println("Hello World!!!");
+    }
+
+    public static void helloCF() {
+        System.out.println("Hello Coding Factory!!!");
     }
 }
